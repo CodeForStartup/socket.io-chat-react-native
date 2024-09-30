ca82c44630d0e0013debfdc180946ba332ab5e4b
Luan Nguyen <luannguyenbkit@gmail.com>
2024-09-30 21:24:55 +0700

    feat: refactor pages

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -1,11 +1,29 @@
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { router, Stack } from "expo-router";
+import { Pressable } from "react-native";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        // headerShown: false,
+        contentStyle: { backgroundColor: "white" },
+        // headerTitle: ""
+        headerLeft: () => (
+          <Pressable onPress={() => router.back()}>
+            <Ionicons name="chevron-back" size={24} color="black" />
+          </Pressable>
+        ),
+        headerShadowVisible: false,
+        headerStyle: {
+          // backgroundColor: "white",
+        },
+        // headerBorderVisible: false,
+      }}
+    >
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="chat/[channelId]" />
-      <Stack.Screen name="chat/add-friend" />
+      <Stack.Screen name="chat/add-friend" options={{ headerShown: false }} />
     </Stack>
   );
 }
